4108810f2ac33e48e5e49765a14286e0e4e2b988
Arnab-1473 <119725146+Arnab-1473@users.noreply.github.com>
2023-03-13 12:37:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 // Your Script here.
 function rot13(str) {
-	const alpahbet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	const shift = 13;
-	let result = '';
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const shift = 13;
+  let result = '';
 
-	for (let i = 0; i < str.length; i++) {
-		const char = str[i];
-		const charIndex = alpahbet.indexof(char);
-//if there is no such alphabet
-		if (charIndex === -1){
-			result = result+char;
-			continue;
-		}
-const shiftedIndex = (charIndex+shift)%alpahbet.length;
-		result = result+alpahbet[shiftedIndex];		
-	}
-	return result;
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    const charIndex = alphabet.indexOf(char);
+
+    // If char is not in the alphabet, pass it through unchanged
+    if (charIndex === -1) {
+      result += char;
+      continue;
+    }
+
+    // Shift the char by the shift amount
+    const shiftedIndex = (charIndex + shift) % alphabet.length;
+    result += alphabet[shiftedIndex];
+  }
+
+  return result;
 }
