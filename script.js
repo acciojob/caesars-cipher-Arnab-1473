f2d4652f44753afa2b47ab39054d5068d62a3a90
Arnab-1473 <119725146+Arnab-1473@users.noreply.github.com>
2023-03-13 12:33:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Your Script here.
+function rot13(str) {
+	const alpahbet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	const shift = 13;
+	let result = '';
+
+	for (let i = 0; i < str.length; i++) {
+		const char = str[i];
+		const charIndex = alpahbet.indexof(char);
+//if there is no such alphabet
+		if (charIndex === -1){
+			result = result+char;
+			continue;
+		}
+const shiftedIndex = (charIndex+shift)%alpahbet.length;
+		result = result+alpahbet[shiftedIndex];		
+	}
+	return result;
+}
